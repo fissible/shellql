ad4c95968689287cfa0901d098db0bfe9f131a18
Allen McCabe <allenmccabe@gmail.com>
2026-03-17 00:12:56 -0700

    Uranium theme: neon green header bar (bold dark text on bright green bg)

--- src/themes/uranium.sh
+++ src/themes/uranium.sh
@@ -5,5 +5,5 @@ SHQL_THEME_PANEL_STYLE="rounded"
 SHQL_THEME_KEY_COLOR=$'\033[32m'           # green
 SHQL_THEME_VALUE_COLOR=$'\033[96m'         # bright cyan
 SHQL_THEME_VALUE_ACCENT_COLOR=$'\033[93m'  # amber/gold
-SHQL_THEME_HEADER_BG=$'\033[7m'            # reverse video
+SHQL_THEME_HEADER_BG=$'\033[1;30;102m'     # bold dark text on bright green background
 SHQL_THEME_RESET=$'\033[0m'
